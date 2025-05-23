dd12285c8f71b405d232e55d6807570ad163e508
Shara2922 <r.shara2922@gmail.com>
2025-05-23 14:06:54 -0500

    Functions

---
+++ Functions.py
@@ -0,0 +1,11 @@
+# 1. Declare a function add_two_numbers. It takes two parameters and it returns a sum.
+def add_two_numbers(a, b):
+    return a + b
+# 2. Declare a function add_three_numbers. It takes three parameters and it returns a sum.
+def add_three_numbers(a, b, c):
+    return a + b + c
+# 3. Area of a circle is calculated as follows: area = π x r x r.
+# Write a function that calculates area_of_circle.
+import math
+def area_of_circle(radius):
+    return math.pi * radius * radius
