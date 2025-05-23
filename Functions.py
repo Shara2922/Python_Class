9e81b53d37a708506a1365367e52c321da89feab
Shara2922 <r.shara2922@gmail.com>
2025-05-23 14:10:40 -0500

    exercise

--- Functions.py
+++ Functions.py
@@ -9,3 +9,10 @@ def add_three_numbers(a, b, c):
 import math
 def area_of_circle(radius):
     return math.pi * radius * radius
+# 4. Write a function called add_all_nums which takes arbitrary number of arguments and sums all the arguments.
+# Check if all the list items are number types. If not do give a reasonable feedback.
+def add_all_nums(*args):
+    if all(isinstance(i, (int, float)) for i in args):
+        return sum(args)
+    else:
+        return "All arguments must be numbers."
