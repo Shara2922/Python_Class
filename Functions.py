f738febf90c784101ed9ad405ad797bac436590d
Shara2922 <r.shara2922@gmail.com>
2025-05-25 09:44:41 -0500

    Functions Practice

--- Functions.py
+++ Functions.py
@@ -16,3 +16,25 @@ def add_all_nums(*args):
         return sum(args)
     else:
         return "All arguments must be numbers."
+    # 5. Temperature in °C can be converted to °F using this formula: °F = (°C x 9/5) + 32.
+    # Write a function which converts °C to °F, convert_celsius_to-fahrenheit.
+def convert_celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+# 6. Write a function called check-season, it takes a month parameter and
+# returns the season: Autumn, Winter, Spring or Summer.
+def check_season(month):
+    if month in [12, 1, 2]:
+        return "Winter"
+    elif month in [3, 4, 5]:
+        return "Spring"
+    elif month in [6, 7, 8]:
+        return "Summer"
+    elif month in [9, 10, 11]:
+        return "Autumn"
+    else:
+        return "Invalid month. Please enter a number between 1 and 12."
+# 7. Declare a function called calculate_slope which returns the slope of a linear function.
+def calculate_slope(y2, y1, x2, x1):
+    if x2 - x1 == 0:
+        return "Slope is undefined (vertical line)."
+    return (y2 - y1) / (x2 - x1)
